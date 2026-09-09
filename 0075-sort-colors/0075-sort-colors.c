0dd7e45b3a485c28eb320261dea2f66f9221f184
Kalpit Tailor <taylorkalpit@gmail.com>
2026-09-10 00:56:49 +0530

    Time: 0 ms (100%), Space: 10.5 MB (74.08%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.c
@@ -0,0 +1,21 @@
+void sortColors(int* nums, int numsSize) {
+    int left=0;
+    int right=numsSize-1;
+    int mid=0;
+    while(mid<=right){
+        if(nums[mid]==0){
+            int temp=nums[left];
+            nums[left]=nums[mid];
+            nums[mid]=temp;
+            left++;
+            mid++;
+        }else if(nums[mid]==1){
+            mid++;
+        }else{
+            int temp=nums[mid];
+            nums[mid]=nums[right];
+            nums[right]=temp;
+            right--;
+        }
+    }
+}
